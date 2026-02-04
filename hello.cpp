753d5af82052b31e06c82545c53e2a6c2f8c6cf5
andymendbayar <andymendbayar@gmail.com>
2026-02-04 10:46:32 -0800

    Add hello.cpp with greeting and points display
    Fix variable initialization and output formatting.

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int points;
+    string name;
+  
+    int points =  17;
+    string name = Andy;
+  
+    cout << "\"Hello, " << name << "! The computer has seen fit to assign you " << points << "points.\"" << endl;
+    return 0;
+}
